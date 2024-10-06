a3cda8444b4b8cccbd525a11ee783b3ef3b8d343
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-10-05 23:30:07 -0400

    created window

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-#test
+from tkinter import *
+import pandas
 
-  
+#initialize the window
+window = Tk()
+
+#@ sizing 
+#screen_width = window.winfo_screenwidth()
+#screen_height = window.winfo_screenheight()
+window.state("zoomed")
+
+
+window.mainloop()
