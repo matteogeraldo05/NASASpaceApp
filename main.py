142c268bb976a3006d4d700668afbc6f126cabaf
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-10-05 23:53:12 -0400

    icon

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ def main():
     #window initializeation
     root = tk.Tk()
     root.title("NASA Space App")
-    root.attributes("-fullscreen", True)
+    root.iconbitmap("images/nasa.ico")
+    root.state("zoomed") #start in windowed fullscreen
+    root.resizable(0,0) #disable resizing
     root.configure(bg="#252526")
 
-    #run main
+    #run
     root.mainloop()
 
 if __name__ == "__main__":
