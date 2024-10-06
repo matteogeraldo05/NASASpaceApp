deda8bef9fb416fcab97d7963a1f3c565a2e759a
ThomasP <thomas.petkovic@ontariotechu.net>
2024-10-05 23:32:56 -0400

    thomas

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from tkinter import *
-import pandas
-
-#initialize the window
-window = Tk()
-
-#@ sizing 
-#screen_width = window.winfo_screenwidth()
-#screen_height = window.winfo_screenheight()
-window.state("zoomed")
+import tkinter as tk
 
 
-window.mainloop()
+def main():
+   
+
+    root = tk.Tk()
+    root.title("Basic GUI Window")
+    root.geometry("1920x1080")  # Move 2 pixels to the left
+    root.configure(bg="#252526")
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+  
