b025ca360c836d25bc6cc19611dd008e0e23f367
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-10-06 00:14:59 -0400

    themed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from ctypes import windll, byref, sizeof, c_int
 import pandas
 
 def main():
@@ -6,10 +7,18 @@ def main():
     root = tk.Tk()
     root.title("NASA Space App")
     root.iconbitmap("images/nasa.ico")
-    root.state("zoomed") #start in windowed fullscreen
+    root.geometry("960x540")
     root.resizable(0,0) #disable resizing
-    root.configure(bg="#252526")
+    
+    color_pallet = ["#121212", "#171717", "#1C1C1C", "#252525", "#383838"]
 
+    #get current window
+    HWND = windll.user32.GetParent(root.winfo_id()) #get window handle from current open window (root)
+    #https://learn.microsoft.com/en-us/windows/win32/api/dwmapi/ne-dwmapi-dwmwindowattribute
+    windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(0x00171717)), sizeof(c_int))
+    root.configure(bg=color_pallet[0])
+
+    
     #run
     root.mainloop()
 
