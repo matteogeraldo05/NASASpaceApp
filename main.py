efd71b60b53aad83af2a9d73bd6212b92a99d194
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-10-06 00:44:54 -0400

    custom font

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font
 #will crash on macOS / linux if try except is removed
 try:
     from ctypes import windll, byref, sizeof, c_int
@@ -13,8 +14,10 @@ def main():
     root.iconbitmap("images/nasa.ico")
     root.geometry("960x540")
     root.resizable(0,0) #disable resizing
-    
+
     color_pallet = ["#121212", "#171717", "#1C1C1C", "#252525", "#383838"]
+    code_font = font.Font(family="Space Mono", size=24)
+
     try:
         #get current window
         HWND = windll.user32.GetParent(root.winfo_id()) #get window handle from current open window (root)
@@ -22,8 +25,11 @@ def main():
         windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(0x00171717)), sizeof(c_int))
     except:
         pass
-    
+
     root.configure(bg=color_pallet[0])
+    #title text
+    title_text = tk.Label(master=root, text="Seismic Detection-inator", font=code_font, fg="white", bg=color_pallet[0])
+    title_text.pack(pady=20)
 
     
     #run
